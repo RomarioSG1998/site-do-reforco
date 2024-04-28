d05c5ba104830b243dd4aa148af690ad0c35b173
RomarioSG1998 <rg1606694@gmail.com>
2024-04-27 21:32:33 -0300

    d

---
+++ Whats.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Imagem no Centro</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #20B2AA; /* Cor de fundo opcional */
+        }
+
+        img {
+            max-width: 100%;
+            max-height: 100%;
+        }
+    </style>
+</head>
+<body>
+    <img src="../imagens/wt.jpeg" alt="Imagem">
+</body>
+</html>
