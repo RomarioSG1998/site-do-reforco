05137afd455e9cb483280315da98f7c06b98b67e
Romário Galdino <149737192+RomarioSG1998@users.noreply.github.com>
2024-05-04 13:03:44 -0300

    Update gerar_pdf.php

--- gerar_pdf.php
+++ gerar_pdf.php
@@ -73,7 +73,7 @@ function generatePDF() {
     $hostname = "localhost";
     $bancodedados = "if0_36181052_sistemadoreforco";
     $usuario = "if0_36181052";
-    $senha = "A7E5zgIppyr";
+    $senha = "";
     $conexao = new mysqli($hostname, $usuario, $senha, $bancodedados);
 
     if ($conexao->connect_error) {
