11ff62a33a35bbe704092b4d96b57e3526c20519
Romário Galdino <149737192+RomarioSG1998@users.noreply.github.com>
2024-05-04 12:56:54 -0300

    Update Monitorar.php

--- Monitorar.php
+++ Monitorar.php
@@ -6,7 +6,7 @@ include('protect.php');
 $hostname = "localhost";
 $bancodedados = "if0_36181052_sistemadoreforco";
 $usuario = "if0_36181052";
-$senha = "A7E5zgIppyr";
+$senha = "";
 
 $conexao = new mysqli($hostname, $usuario, $senha, $bancodedados);
 
